149628edd248994aec2eba87fa130b1cf87b9314
Gizem Kurtuluş <98229260+gizem5@users.noreply.github.com>
2022-09-28 18:03:21 +0300

    EnginDemirogJava2022Kampı
    EnginDemirogJava2022Kampı

---
+++ classesWithAttributes/src/classesWithAttributes/ClassesWithAttributes.java
@@ -0,0 +1,17 @@
+package classesWithAttributes;
+
+public class ClassesWithAttributes {
+
+	public static void main(String[] args) {
+		Product product = new Product(1, "Laptop", "Asus Laptop", 3000, 2, "Siyah");
+
+
+		
+		
+		
+		ProductManager productManager = new ProductManager();
+		productManager.Add(product);
+		System.out.println(product.getKod());
+
+	}
+}
---
+++ inheritanceDemo/src/inheritanceDemo/İnheritanceDemo.java
@@ -0,0 +1,10 @@
+package inheritanceDemo;
+
+public class İnheritanceDemo {
+
+	public static void main(String[] args) {
+		KrediUI krediUI = new KrediUI();
+		krediUI.KrediHesapla(new AskerKrediManager());
+	}
+
+}
